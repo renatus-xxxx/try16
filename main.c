0d0f794993278a924d99b515cabb1fa0a0f859b0
nnkgw <148290899+renatus-xxxx@users.noreply.github.com>
2025-06-14 21:34:49 +0900

    optimize

--- main.c
+++ main.c
@@ -12,20 +12,25 @@ int main(int argc, char* argv[]) {
   clock_t last  = start;
   unsigned int count = 0;
   unsigned char prev = 0;
+  uint8_t show_flag = 0;
   while (platform_elapsed_ticks(start, platform_clock()) < 600) {
     unsigned char trig = platform_gttrig(1);
     if (!prev && trig) count++;
     prev = trig;
     now = platform_clock();
     if (platform_elapsed_ticks(last, now) >= 60) {
-      uint16_t ticks = platform_elapsed_ticks(start, now);
-      int elapsed10 = (ticks * 10) / CLOCKS_PER_SEC;
-      printf("\rElapsed: %d.%d sec   Count: %d     ", elapsed10 / 10, elapsed10 % 10, count);
-      fflush(stdout);
+      show_flag = 1;
       last = now;
     }
+    if (show_flag) {
+      show_flag = 0;
+      uint16_t ticks = platform_elapsed_ticks(start, now);
+      int elapsed10 = (ticks * 10) / CLOCKS_PER_SEC;
+      printf("\rElapsed: %d.%d sec Count: %d     ", elapsed10 / 10, elapsed10 % 10, count);
+      fflush(stdout);
+    }
   }
-  printf("\nAvg %.2f shots/sec\n", count / 10.0f);
+  printf("Avg %.2f shots/sec\n", count / 10.0f);
   printf("Done.");
   return 0;
 }
