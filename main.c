104adb3d1c5ba3c76603389fb5e25323c02101b5
nnkgw <148290899+renatus-xxxx@users.noreply.github.com>
2025-06-14 21:06:24 +0900

    optimize on msx

--- main.c
+++ main.c
@@ -7,28 +7,27 @@
 
 int main(int argc, char* argv[]) {
   printf("Try16: A retro-style rapid fire counter.\n");
-  clock_t start       = platform_clock();
-  clock_t last        = platform_clock();
-  float   elapsed_sec = platform_elapsed(start, last);
-  unsigned char prev_trig = 0;
-  unsigned int  count     = 0;
-  while (elapsed_sec < 10.0f) {
-    clock_t now   = platform_clock();
-    while(platform_elapsed(last, now) < (1.0f / 60.0f)) { // NTSC Only
-      now = platform_clock(); // wait vsync (60FPS)
-    }
-    last = platform_clock();
-    elapsed_sec = platform_elapsed(start, now);
+
+  clock_t start = platform_clock();
+  clock_t now, last = start;
+
+  unsigned int count = 0;
+  unsigned char prev = 0;
+
+  while (platform_elapsed_ticks(start, platform_clock()) < 600) {
     unsigned char trig = platform_gttrig(1);
-    if (!prev_trig && trig) {
-      count++;
+    if (!prev && trig) count++;
+    prev = trig;
+    now = platform_clock();
+    if (platform_elapsed_ticks(last, now) >= 60) {
+      uint16_t ticks = platform_elapsed_ticks(start, now);
+      int elapsed10 = (ticks * 10) / CLOCKS_PER_SEC;
+      printf("\rElapsed: %d.%d sec   Count: %d     ", elapsed10 / 10, elapsed10 % 10, count);
+      fflush(stdout);
+      last = now;
     }
-    prev_trig = trig;
-    printf("\rElapsed: %.1f sec   Count: %d      ", elapsed_sec, count);
-    fflush(stdout);
   }
-  float avg = count / 10.0f;
-  printf("\nAvg %.2f shots/sec\n", avg);
-  printf("\nDone.\n");
+  printf("\nAvg %.2f shots/sec\n", count / 10.0f);
+  printf("Done.");
   return 0;
 }
--- platform.h
+++ platform.h
@@ -4,8 +4,22 @@
 #include <stdint.h>
 
 #ifdef MSX
-#define CLOCKS_PER_SEC 60
-#define JIFFY          0xfc9e
+
+#define CLOCKS_PER_SEC (60)
+#define JIFFY          (0xfc9e)
+
+clock_t platform_clock(void) {
+  uint8_t* ptr = (uint8_t*)JIFFY;
+  return (clock_t)(ptr[0] | (ptr[1] << 8));
+}
+
+uint16_t platform_elapsed_ticks(clock_t start, clock_t end) {
+  return (uint16_t)((end - start) & 0xFFFF);
+}
+
+float platform_elapsed_sec(clock_t start, clock_t end) {
+  return platform_elapsed_ticks(start, end) / (float)CLOCKS_PER_SEC;
+}
 
 struct regs {
   char     f;
@@ -15,20 +29,18 @@ struct regs {
   unsigned hl;
 };
 
-void calbio(unsigned int address, struct regs *r) {
-
+void calbio(unsigned int address, struct regs* r) {
 #asm
-
 CALBIO_CALSLT	equ	001ch
-CALBIO_EXPTBL	equ	0fcc1h		;slot address of BIOS ROM
+CALBIO_EXPTBL	equ	0fcc1h
 
     ld	hl,2
 	add	hl,sp
-	ld	e,(hl)	;second param to de
+	ld	e,(hl)
 	inc	hl
 	ld	d,(hl)
 	inc	hl
-	ld	c,(hl)	;first param to hl
+	ld	c,(hl)
 	inc	hl
 	ld	b,(hl)
 	ld  l,c
@@ -70,36 +82,36 @@ CALBIO01:
 #endasm
 }
 
-clock_t platform_clock(void) {
-  uint8_t* ptr = (uint8_t*)0xFC9E;
-  return (clock_t)(ptr[0] | (ptr[1] << 8));
-}
-
-float platform_elapsed(clock_t start, clock_t end) {
-  return ((end - start) & 0xFFFF) / (float)CLOCKS_PER_SEC;
-}
-
-unsigned char platform_gttrig(char no) {
+unsigned char platform_gttrig(int no) {
   struct regs r;
-  r.a = no;
-  calbio(0x00d8, &r); // MSX BIOS call 0x00D8 = GTTRIG
+  r.a = (char)no;
+  calbio(0x00d8, &r);
   return (r.a != 0);
 }
-#else
+
+#else // Non-MSX
+
 #include <time.h>
+
+//typedef clock_t clock_t;
 
 clock_t platform_clock(void) {
   return clock();
 }
 
-float platform_elapsed(clock_t start, clock_t end) {
+float platform_elapsed_sec(clock_t start, clock_t end) {
   float sec = (float)(end - start) / CLOCKS_PER_SEC;
-  return (sec <= 0.0f) ? (float)1e-6 : sec;
+  return (sec <= 0.0f) ? 1e-6f : sec;
 }
 
-unsigned char platform_gttrig(int no) {
-  return 0;
+uint16_t platform_elapsed_ticks(clock_t start, clock_t end) {
+  return (uint16_t)(platform_elapsed_sec(start, end) * 60.0f);
 }
+
+static inline unsigned char platform_gttrig(int no) {
+  return 0; // dummy trigger for non-MSX
+}
+
 #endif
 
 #endif // PLATFORM_H
